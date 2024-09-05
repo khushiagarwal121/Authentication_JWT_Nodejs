a9faa8aa6b5a4bb2b3684ba4c7c8ee6b2666231e
KhushiA <khushi.agarwal@argusoft.in>
2024-09-05 14:28:07 +0530

    added logger

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
+// Setup logging in server.js
+const morgan = require("morgan");
+const logger = require("./config/logger");
 
 const app = express();
-
+app.use(morgon("combined"));
 connectDB();
 
 app.use(bodyParser.json());
@@ -14,6 +17,7 @@ app.use("/api/auth", require("./routes/auth"));
 
 // Error-handling middleware (after all other middleware and routes)
 app.use((err, req, res, next) => {
+  logger.error(err.stack); // Log error using winston
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
